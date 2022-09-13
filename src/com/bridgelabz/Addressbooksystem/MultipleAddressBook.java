915a4f328c7ddd986ef5867063ed1c4f29cb0911
shashi <shashidharngg@gmail.com>
2022-09-13 14:37:27 +0530

    added:View Person Uc9

--- src/com/bridgelabz/Addressbooksystem/MultipleAddressBook.java
+++ src/com/bridgelabz/Addressbooksystem/MultipleAddressBook.java
@@ -7,47 +7,62 @@ import java.util.Scanner;
 
 public class MultipleAddressBook {
 	private Map<String,MultipleContacts> addressBookMap;
-	private MultipleContacts addressb;
+	private Map<String,MultipleContacts> cityMap;
+	private Map<String,MultipleContacts> stateMap;
+	MultipleContacts addressb;
+	MultipleContacts cityb;
+	MultipleContacts stateb;
 	Scanner sc;
 	public MultipleAddressBook() {
 		addressBookMap=new HashMap<>();
+		cityMap=new HashMap<>();
+		stateMap=new HashMap<>();
 		addressb=new MultipleContacts();
+		cityb=null;
+		stateb=null;
 		sc=new Scanner(System.in);
+		
 	}
 	public int displayChoice() {
 		System.out.println("1.Add new Address Book");
 		System.out.println("2.View Address Book List");
-		System.out.println("3.Select Address Book to View Contact Details");
-		System.out.println("4.Search Person By City");
-		System.out.println("5.Search Person By State");
-		System.out.println("6.Quit");
-		System.out.println("Enter choice from 1 and 6");
+		System.out.println("3.Search Person By City");
+		System.out.println("4.Search Person By State");
+		System.out.println("5.Quit");
+		System.out.println("Enter choice from 1 and 5");
 		int choice=sc.nextInt();
 		return choice;
 	}
 	public void addNewAddressBook() {
-		System.out.println("Enter City Name For Address Book");
+		System.out.println("Enter Name For Address Book");
 		String adbName=sc.next();
 		adbName=adbName.toLowerCase();
-		addressb.addContact();
-		addressBookMap.put(adbName, addressb);
+		Contacts cc=addressb.addContact();
+		addressBookMap.put(aName, addressb);
+		if(cityMap.containsKey(cc.city)) {
+			cityb.contactArrayList.add(cc);
+			cityMap.put(cc.city, cityb);
+		} else {
+			cityb=new MultipleContacts();
+			cityb.contactArrayList.add(cc);
+			cityMap.put(cc.city, cityb);
+		}
+		if(stateMap.containsKey(cc.state)) {
+			stateb.contactArrayList.add(cc);
+			stateMap.put(cc.state, stateb);
+		} else {
+			stateb=new MultipleContacts();
+			stateb.contactArrayList.add(cc);
+			stateMap.put(cc.state, stateb);
+		}
 	}
+	
 	public void viewAddressBookList() {
 		for(Map.Entry<String,MultipleContacts> m:addressBookMap.entrySet()) {
 			System.out.println(m.getKey());
 		}
 	}
-	public void selectAddressBook() {
-		System.out.println("Enter Name of Address Book to View Contact");
-		String adbName=sc.next();
-		adbName=adbName.toLowerCase();
-		for(Map.Entry<String,MultipleContacts> m:addressBookMap.entrySet()) {
-			if(m.getKey().equals(adbName)) {
-				addressb.performContactOperation((MultipleContacts)m.getValue());
-			}
-		}
-		}
-
+	
 		public void searchContactByCityName() {
 			System.out.println("Enter name of City to search contact");
 			String city=sc.next();
@@ -75,6 +90,15 @@ public class MultipleAddressBook {
 				nc.showAllContacts();
 			}
 	}
+		public void viewContactByState() {
+			System.out.println("Enter name of State to search contact");
+			String state=sc.next();
+			state=state.toLowerCase();
+			if(stateMap.containsKey(state)) {
+				MultipleContacts list=stateMap.get(state);
+				list.showAllContacts();
+			}
+		}
 	public static void main(String[] args) {
 		MultipleAddressBook mab=new MultipleAddressBook();
 		int choice;
@@ -89,20 +113,17 @@ public class MultipleAddressBook {
 					mab.viewAddressBookList();
 					break;
 				case 3:
-					mab.selectAddressBook();
-					break;
-				case 4:
 					mab.searchContactByCityName();
 					break;
-				case 5:
-					mab.searchContactByStateName();
+				case 4:
+					mab.viewContactByState();
 					break;
 					
-				case 6:
+				case 5:
 					flag=1;
 					break;
 				default:
-					System.out.println("You have Entered Wrong Choice.Please enter option between 1 to 6");
+					System.out.println("You have Entered Wrong Choice.Please enter option between 1 to 5");
 			}
 		}
 	}
